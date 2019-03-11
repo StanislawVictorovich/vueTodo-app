f8e761a3c3ca844db2a2b46e6abab5bca9b21e8d
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-11 10:17:21 +0200

    error handler

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const URL = 'https://jsonplaceholder.typicode.com/todos/';
+export const APP_NAME = 'ToDo-App on VUE';
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { APP_NAME } from './constants'
 
 Vue.config.productionTip = false
-
+Vue.config.errorHandler = function (error, vm, info) {
+  console.warn(APP_NAME, error, vm, info);
+}
 
 new Vue({
   store,
