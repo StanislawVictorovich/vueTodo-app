182f8141009e2c5520202707a9767a130ec86514
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-12 10:00:06 +0200

    ----

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from './App.vue'
 import store from './store'
 import { APP_NAME } from './constants'
 
-Vue.config.errorHandler = function (error, vm, info) {
-  console.warn(APP_NAME, error, vm, info);
-}
+Vue.config.errorHandler = (error, vm, info) => console.warn(APP_NAME, error, vm, info);
 
 new Vue({
   store,
